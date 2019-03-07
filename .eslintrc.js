71464f7bc6444e40db9bc107254a4137742ab1ed
kobi.carmeli <kobi.carmeli@zooz.com>
2019-03-07 17:23:15 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "node": true,
+        "jest": true,
+        "mocha": true
+    },
+    "rules": {
+        "camelcase": "off",
+        "no-underscore-dangle": "off",
+    }
+}
