d7f00d1442b2cb0ff169f9918d160c10a0096016
kobi.carmeli <kobi.carmeli@zooz.com>
2019-03-08 10:29:38 +0200

    increase coverage

--- test/request.test.js
+++ test/request.test.js
@@ -1,25 +1,23 @@
-const rp = require('request-promise-native');
-const nock = require('nock');
 const chai = require('chai');
 
 const apiSchema = require('../lib/index');
+const generateResponse = require('./helpers/response-generator');
 
 chai.use(apiSchema);
 const { expect } = chai;
 
 describe('When the response object matches the schema', () => {
   it('Should not throw an exception', async () => {
-    nock(/./).get('/').reply(200, {
-      firstName: 'Adam',
-      lastName: 'Driver',
-      age: 30,
-    }, {
-      'content-type': 'application-json',
-      'x-request-id': 'id',
-    });
-    const response = await rp.get('http://www.dsasdsa.com', {
-      resolveWithFullResponse: true,
-      json: true,
+    const response = await generateResponse(200, {
+      body: {
+        firstName: 'Adam',
+        lastName: 'Driver',
+        age: 30,
+      },
+      headers: {
+        'content-type': 'application-json',
+        'x-request-id': 'id',
+      },
     });
 
     expect(response).to.be.successful().and.to.matchApiSchema(schema);
@@ -28,24 +26,39 @@ describe('When the response object matches the schema', () => {
 
 describe('When the response object does not match the schema', () => {
   it('Should throw an exception', async () => {
-    nock(/./).get('/').reply(200, {
-      lastName: 'Driver',
-      age: -5,
-    }, {
-      'content-type': 'application-json',
-      'x-request-id': 'id',
-    });
-
-    const response = await rp.get('http://www.dsasdsa.com', {
-      resolveWithFullResponse: true,
-      json: true,
+    const response = await generateResponse(200, {
+      body: {
+        lastName: 'Driver',
+        age: -5,
+      },
+      headers: {
+        'content-type': 'application-json',
+        'x-request-id': 'id',
+      },
     });
 
     expect(response).to.not.matchApiSchema(schema);
   });
 });
 
-const schema = {
+// TODO: test mapErrorsByDataPath directly instead
+describe('When the response is missing multiple required props', () => {
+  it('Should return an array of required props', async () => {
+    const response = await generateResponse(200, {
+      body: {
+        age: -5,
+      },
+      headers: {
+        'content-type': 'application-json',
+        'x-request-id': 'id',
+      },
+    });
+
+    expect(response).to.not.matchApiSchema(schema);
+  });
+});
+
+const schema = Object.freeze({
   type: 'object',
   properties: {
     firstName: {
@@ -64,4 +77,4 @@ const schema = {
     },
   },
   required: ['firstName', 'lastName'],
-};
+});
--- test/status-code-matcher.test.js
+++ test/status-code-matcher.test.js
@@ -1,50 +1,50 @@
 const chai = require('chai');
 
 const apiSchema = require('../lib/index');
-const responseGen = require('./response-generator');
+const generateResponse = require('./helpers/response-generator');
 
 chai.use(apiSchema);
 const { expect } = chai;
 
 describe('Status code matcher', () => {
   it('successful', async () => {
-    const response = await responseGen(200);
+    const response = await generateResponse(200);
     expect(response).to.be.successful();
   });
   it('created', async () => {
-    const response = await responseGen(201);
+    const response = await generateResponse(201);
     expect(response).to.be.created();
   });
   it('badRequest', async () => {
-    const response = await responseGen(400);
+    const response = await generateResponse(400);
     expect(response).to.be.badRequest();
   });
   it('unauthorized', async () => {
-    const response = await responseGen(401);
+    const response = await generateResponse(401);
     expect(response).to.be.unauthorized();
   });
   it('rejected', async () => {
-    const response = await responseGen(403);
+    const response = await generateResponse(403);
     expect(response).to.be.rejected();
   });
   it('notFound', async () => {
-    const response = await responseGen(404);
+    const response = await generateResponse(404);
     expect(response).to.be.notFound();
   });
   it('serverError', async () => {
-    const response = await responseGen(500);
+    const response = await generateResponse(500);
     expect(response).to.be.serverError();
   });
   it('serviceUnavailable', async () => {
-    const response = await responseGen(503);
+    const response = await generateResponse(503);
     expect(response).to.be.serviceUnavailable();
   });
   it('gatewayTimeout', async () => {
-    const response = await responseGen(504);
+    const response = await generateResponse(504);
     expect(response).to.be.gatewayTimeout();
   });
   it('custom code', async () => {
-    const response = await responseGen(204);
+    const response = await generateResponse(204);
     expect(response).to.have.status(204);
   });
 });
