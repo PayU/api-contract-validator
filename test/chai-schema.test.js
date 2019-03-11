2352cc6e52754e48768e1df8b00650c7ab93b094
kobi.carmeli <kobi.carmeli@zooz.com>
2019-03-11 17:49:46 +0200

    arrange tests. prepare code for more assertion libraries

--- test/chai-schema.test.js
+++ test/chai-schema.test.js
@@ -1,17 +1,17 @@
 const chai = require('chai');
 
 const apiSchema = require('../lib/index').chaiPlugin;
-const generateResponse = require('./helpers/response-generator').supertest;
+const { request } = require('./helpers/response-generator');
 const { schema } = require('./helpers/schemas');
 const { headersObject, bodyObject } = require('./helpers/data');
 
 chai.use(apiSchema);
 const { expect } = chai;
 
-describe('supertest', () => {
+describe('chai plugin test', () => {
   describe('When the response object matches the schema', () => {
     it('Should not throw an exception', async () => {
-      const response = await generateResponse({
+      const response = await request({
         status: 200,
         body: bodyObject.valid,
         headers: headersObject.valid,
@@ -22,14 +22,14 @@ describe('supertest', () => {
   });
 
   describe('When the response object does not match the schema', () => {
-    it('Should throw an exception', async () => {
-      const response = await generateResponse({
+    it('Should not match the schema', async () => {
+      const response = await request({
         status: 200,
         body: bodyObject.invalid,
-        headers: headersObject.invalid,
+        headers: headersObject.valid,
       });
 
-      expect(response).to.not.matchApiSchema(schema);
+      expect(response).to.be.successful().and.to.not.matchApiSchema(schema);
     });
   });
 });
